0d29d6d5717c579695cebdd8e6cc01800149b11d
qiao_zhi <qiao_liqiang@163.com>
2019-04-02 20:31:12 +0800

    添加明细

--- src/main/java/cn/qs/controller/DietController.java
+++ src/main/java/cn/qs/controller/DietController.java
@@ -4,23 +4,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.qs.bean.diet.DietWithBLOBs;
+import cn.qs.scheduled.Scheduled;
 import cn.qs.service.diet.DietService;
+import cn.qs.utils.JSONResultUtil;
 
 @Controller
 public class DietController {
 
+	private static final Logger logger = LoggerFactory.getLogger(DietController.class);
+
 	@Autowired
 	private DietService dietService;
+
+	@Autowired
+	private Scheduled scheduled;
 
 	@RequestMapping("/diet_list")
 	public String diet_list() {
 		return "diet_list";
+	}
+
+	/**
+	 * 手动同步数据，用于测试爬虫
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/syncData")
+	@ResponseBody
+	public JSONResultUtil syncData() {
+		try {
+			scheduled.scheduledMethod();
+		} catch (Exception e) {
+			logger.error("syncData error", e);
+			return JSONResultUtil.error("爬数据出错");
+		}
+
+		return JSONResultUtil.ok();
 	}
 
 	@RequestMapping("/getDietsByKeyword")
---
+++ src/main/java/cn/qs/utils/MySimHash.java
@@ -0,0 +1,197 @@
+package cn.qs.utils;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import com.hankcs.hanlp.seg.common.Term;
+import com.hankcs.hanlp.tokenizer.StandardTokenizer;
+
+public class MySimHash {
+    private String tokens; //字符串
+    private BigInteger strSimHash;//字符产的hash值
+    private int hashbits = 64; // 分词后的hash数;
+
+
+    public MySimHash(String tokens) {
+        this.tokens = tokens;
+        this.strSimHash = this.simHash();
+    }
+
+    private MySimHash(String tokens, int hashbits) {
+        this.tokens = tokens;
+        this.hashbits = hashbits;
+        this.strSimHash = this.simHash();
+    }
+
+
+    /**
+     * 清除html标签
+     * @param content
+     * @return
+     */
+    private String cleanResume(String content) {
+        // 若输入为HTML,下面会过滤掉所有的HTML的tag
+        content = Jsoup.clean(content, Whitelist.none());
+        content = StringUtils.lowerCase(content);
+        String[] strings = {" ", "\n", "\r", "\t", "\\r", "\\n", "\\t", "&nbsp;"};
+        for (String s : strings) {
+            content = content.replaceAll(s, "");
+        }
+        return content;
+    }
+
+
+    /**
+     * 这个是对整个字符串进行hash计算
+     * @return
+     */
+    private BigInteger simHash() {
+
+        tokens = cleanResume(tokens); // cleanResume 删除一些特殊字符
+
+        int[] v = new int[this.hashbits];
+
+        List<Term> termList = StandardTokenizer.segment(this.tokens); // 对字符串进行分词
+
+        //对分词的一些特殊处理 : 比如: 根据词性添加权重 , 过滤掉标点符号 , 过滤超频词汇等;
+        Map<String, Integer> weightOfNature = new HashMap<String, Integer>(); // 词性的权重
+        weightOfNature.put("n", 2); //给名词的权重是2;
+        Map<String, String> stopNatures = new HashMap<String, String>();//停用的词性 如一些标点符号之类的;
+        stopNatures.put("w", ""); //
+        int overCount = 5; //设定超频词汇的界限 ;
+        Map<String, Integer> wordCount = new HashMap<String, Integer>();
+
+        for (Term term : termList) {
+            String word = term.word; //分词字符串
+
+            String nature = term.nature.toString(); // 分词属性;
+            //  过滤超频词
+            if (wordCount.containsKey(word)) {
+                int count = wordCount.get(word);
+                if (count > overCount) {
+                    continue;
+                }
+                wordCount.put(word, count + 1);
+            } else {
+                wordCount.put(word, 1);
+            }
+
+            // 过滤停用词性
+            if (stopNatures.containsKey(nature)) {
+                continue;
+            }
+
+            // 2、将每一个分词hash为一组固定长度的数列.比如 64bit 的一个整数.
+            BigInteger t = this.hash(word);
+            for (int i = 0; i < this.hashbits; i++) {
+                BigInteger bitmask = new BigInteger("1").shiftLeft(i);
+                // 3、建立一个长度为64的整数数组(假设要生成64位的数字指纹,也可以是其它数字),
+                // 对每一个分词hash后的数列进行判断,如果是1000...1,那么数组的第一位和末尾一位加1,
+                // 中间的62位减一,也就是说,逢1加1,逢0减1.一直到把所有的分词hash数列全部判断完毕.
+                int weight = 1;  //添加权重
+                if (weightOfNature.containsKey(nature)) {
+                    weight = weightOfNature.get(nature);
+                }
+                if (t.and(bitmask).signum() != 0) {
+                    // 这里是计算整个文档的所有特征的向量和
+                    v[i] += weight;
+                } else {
+                    v[i] -= weight;
+                }
+            }
+        }
+        BigInteger fingerprint = new BigInteger("0");
+        for (int i = 0; i < this.hashbits; i++) {
+            if (v[i] >= 0) {
+                fingerprint = fingerprint.add(new BigInteger("1").shiftLeft(i));
+            }
+        }
+        return fingerprint;
+    }
+
+
+    /**
+     * 对单个的分词进行hash计算;
+     * @param source
+     * @return
+     */
+    private BigInteger hash(String source) {
+        if (source == null || source.length() == 0) {
+            return new BigInteger("0");
+        } else {
+            /**
+             * 当sourece 的长度过短，会导致hash算法失效，因此需要对过短的词补偿
+             */
+            while (source.length() < 3) {
+                source = source + source.charAt(0);
+            }
+            char[] sourceArray = source.toCharArray();
+            BigInteger x = BigInteger.valueOf(((long) sourceArray[0]) << 7);
+            BigInteger m = new BigInteger("1000003");
+            BigInteger mask = new BigInteger("2").pow(this.hashbits).subtract(new BigInteger("1"));
+            for (char item : sourceArray) {
+                BigInteger temp = BigInteger.valueOf((long) item);
+                x = x.multiply(m).xor(temp).and(mask);
+            }
+            x = x.xor(new BigInteger(String.valueOf(source.length())));
+            if (x.equals(new BigInteger("-1"))) {
+                x = new BigInteger("-2");
+            }
+            return x;
+        }
+    }
+
+    /**
+     * 计算海明距离,海明距离越小说明越相似;
+     * @param other
+     * @return
+     */
+    private int hammingDistance(MySimHash other) {
+        BigInteger m = new BigInteger("1").shiftLeft(this.hashbits).subtract(
+                new BigInteger("1"));
+        BigInteger x = this.strSimHash.xor(other.strSimHash).and(m);
+        int tot = 0;
+        while (x.signum() != 0) {
+            tot += 1;
+            x = x.and(x.subtract(new BigInteger("1")));
+        }
+        return tot;
+    }
+
+
+    public double getSemblance(MySimHash s2 ){
+        double i = (double) this.hammingDistance(s2);
+        return 1 - i/this.hashbits ;
+    }
+
+    public static void main(String[] args) {
+
+/*        String s1="simhash算法的主要思想是降维，将高维的特征向量映射成一个低维的特征向量，通过两个向量的Hamming Distance来确定文章是否重复或者高度近似。";
+        String s2="simhash算法的主要思想是降维，将高维的特征向量映射成一个低维的特征向量，通过两个向量的Hamming Distance来确定文章是否重复或者高度近似。";
+        String s3="SimHash算法是Google公司进行海量网页去重的高效算法，它通过将原始的文本映射为64位的二进制数字串，然后通过比较二进制数字串的差异进而来表示原始文本内容的差异。";
+        long l3 = System.currentTimeMillis();
+        MySimHash hash1 = new MySimHash(s1, 64);
+        MySimHash hash2 = new MySimHash(s2, 64);
+        MySimHash hash3 = new MySimHash(s3, 64);
+        System.out.println("======================================");
+        System.out.println(  hash1.hammingDistance(hash2) );
+        System.out.println(  hash2.hammingDistance(hash3) );
+
+        System.out.println(  hash1.getSemblance(hash3) );
+        System.out.println(  hash2.getSemblance(hash3) );
+
+        long l4 = System.currentTimeMillis();
+        System.out.println("总共耗时:"+(l4-l3)+"毫秒");
+        System.out.println("======================================");*/
+    	String s1 = "前列腺";
+    	String s2 = "有问题";
+        MySimHash hash1 = new MySimHash(s1, 64);
+        MySimHash hash2 = new MySimHash(s2, 64);
+        System.out.println(  hash1.hammingDistance(hash2) );
+
+
+    }
+}
